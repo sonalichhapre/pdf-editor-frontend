206894d75529484ab3d93e5df3dc81236644941a
sonalichhapre <sonalichhapre@gmail.com>
2026-02-25 18:14:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8002',
+        target: 'https://pdf-editor-backend-1.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
