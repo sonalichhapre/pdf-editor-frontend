1eefd04c9ae525725d7929e5c24b120431583b08
sonalichhapre <sonalichhapre@gmail.com>
2026-03-16 19:35:06 +0530

    Update ReduceSize.jsx

--- src/pages/ReduceSize.jsx
+++ src/pages/ReduceSize.jsx
@@ -9,7 +9,6 @@ import PageFaq from '../components/PageFaq';
 import ToolPageCta from '../components/ToolPageCta';
 import { PAGE_FAQS, TOOL_WHY_USE } from '../data/pageContent';
 import './ToolPage.css';
-import { useLiveUserCount } from '../hooks/useLiveUserCount';
 
 export default function ReduceSize() {
   const { loading, setLoading, error, setError, canConvert, onSuccess } = useConverter();
